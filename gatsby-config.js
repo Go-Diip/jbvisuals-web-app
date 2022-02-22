2aa77697f3a413f7d9c5d2b5e9f4437bbbf52fb9
rick-as <rick@aftershock.agency>
2022-02-21 23:59:32 -0500

    feat: update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,144 +1,144 @@
 let activeEnv =
-    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 
 console.log(`Using environment config: '${activeEnv}'`)
 
 require("dotenv").config({
-    path: `.env.${activeEnv}`,
+  path: `.env.${activeEnv}`,
 })
 
 console.log(`This WordPress Endpoint is used: '${process.env.WORDPRESS_URL}'`)
 
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.domain.com`,
+  siteMetadata: {
+    siteUrl: `https://www.domain.com`,
+  },
+  flags: {
+    // FAST_DEV: true,
+    DEV_SSR: false,
+    // QUERY_ON_DEMAND: true,
+    // FAST_REFRESH: true,
+    // LAZY_IMAGES: true,
+  },
+  plugins: [
+    // `gatsby-plugin-preact`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-gatsby-cloud`,
+    // Make sure this plugin is first in the array of plugins
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-111111111-1",
+    //     // this option places the tracking script into the head of the DOM
+    //     head: true,
+    //     // other options
+    //   },
+    // },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://www.domain.com",
+        sitemap: "https://www.domain.com/sitemap.xml",
+        env: {
+          development: {
+            policy: [
+              {
+                userAgent: "*",
+                disallow: ["/"],
+              },
+            ],
+          },
+          production: {
+            policy: [
+              {
+                userAgent: "*",
+                allow: "/",
+                disallow: ["/layouts"],
+              },
+            ],
+          },
+        },
+      },
     },
-    flags: {
-        // FAST_DEV: true,
-        DEV_SSR: false,
-        // QUERY_ON_DEMAND: true,
-        // FAST_REFRESH: true,
-        // LAZY_IMAGES: true,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
     },
-    plugins: [
-        // `gatsby-plugin-preact`,
-        `gatsby-plugin-sitemap`,
-        `gatsby-plugin-gatsby-cloud`,
-        // Make sure this plugin is first in the array of plugins
-        {
-            resolve: `gatsby-plugin-google-analytics`,
-            options: {
-                trackingId: "UA-111111111-1",
-                // this option places the tracking script into the head of the DOM
-                head: true,
-                // other options
-            },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-theme-material-ui`,
+      options: {
+        webFontsConfig: {
+          fonts: {
+            google: [
+              {
+                family: `Lato`,
+                variants: [`300`, `400`, `500`, `700`],
+              },
+            ],
+          },
         },
-        {
-            resolve: "gatsby-plugin-robots-txt",
-            options: {
-                host: "https://www.domain.com",
-                sitemap: "https://www.domain.com/sitemap.xml",
-                env: {
-                    development: {
-                        policy: [
-                            {
-                                userAgent: "*",
-                                disallow: ["/"],
-                            },
-                        ],
-                    },
-                    production: {
-                        policy: [
-                            {
-                                userAgent: "*",
-                                allow: "/",
-                                disallow: ["/layouts"],
-                            },
-                        ],
-                    },
-                },
-            },
-        },
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `assets`,
-                path: `${__dirname}/src/assets`,
-            },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: `${__dirname}/src/images`,
-            },
-        },
-        `gatsby-plugin-image`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-transition-link`,
-        {
-            resolve: `gatsby-theme-material-ui`,
-            options: {
-                webFontsConfig: {
-                    fonts: {
-                        google: [
-                            {
-                                family: `Lato`,
-                                variants: [`300`, `400`, `500`, `700`],
-                            },
-                        ],
-                    },
-                },
-            },
-        },
-        `gatsby-plugin-styled-components`,
-        // {
-        //   resolve: `gatsby-plugin-manifest`,
-        //   options: {
-        //     name: `gatsby-starter-default`,
-        //     short_name: `starter`,
-        //     start_url: `/`,
-        //     background_color: `transparent`,
-        //     theme_color: `#39C33A`,
-        //     // icon: `src/assets/icons/short-logo.svg` // This path is relative to the root of the site.
-        //   },
-        // },
+      },
+    },
+    `gatsby-plugin-styled-components`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `transparent`,
+    //     theme_color: `#39C33A`,
+    //     // icon: `src/assets/icons/short-logo.svg` // This path is relative to the root of the site.
+    //   },
+    // },
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from WordPress.
+     */
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
         /*
-         * Gatsby's data processing layer begins with “source”
-         * plugins. Here the site sources its data from WordPress.
+         * The full URL of the WordPress site's GraphQL API.
+         * Example : 'https://www.example-site.com/graphql'
          */
-        {
-            resolve: `gatsby-source-wordpress`,
-            options: {
-                /*
-                 * The full URL of the WordPress site's GraphQL API.
-                 * Example : 'https://www.example-site.com/graphql'
-                 */
-                url: `${process.env.WORDPRESS_URL}/graphql`,
-            },
-        },
+        url: `${process.env.WORDPRESS_URL}/graphql`,
+      },
+    },
 
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                rule: {
-                    include: /assets/,
-                },
-            },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
         },
+      },
+    },
 
-        // {
-        //   resolve: `gatsby-plugin-google-fonts`,
-        //   options: {
-        //     fonts: [
-        //       `DM Sans\:100,400,500,700`,
-        //       `Merriweather\:100,300,400,600,700`,
-        //     ],
-        //     display: "swap",
-        //   },
-        // },
-    ],
+    // {
+    //   resolve: `gatsby-plugin-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       `DM Sans\:100,400,500,700`,
+    //       `Merriweather\:100,300,400,600,700`,
+    //     ],
+    //     display: "swap",
+    //   },
+    // },
+  ],
 }
